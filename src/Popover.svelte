ecf213e3781079e21dd7795b7f50092b442436eb
jguze <jaguze@gmail.com>
2021-06-05 21:57:35 -0700

    Initial commit

---
+++ src/Popover.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<div>{name}</div>
---
+++ src/stories/Popover.stories.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Popover from "../Popover.svelte";
+
+</script>
+
+<Meta
+  title="Example/Popover"
+  component={Popover}
+  argTypes={{
+    name: { control: "text" },
+    primary: { control: "boolean" },
+    backgroundColor: { control: "color" },
+    size: {
+      control: { type: "select", options: ["small", "medium", "large"] },
+    },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Popover {...args} />
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    name: "Primary",
+  }}
+/>
